d1c57160064d686e53f78a7fe66955c930a9cc3f
Yue <38692767+Aaayue@users.noreply.github.com>
2018-06-26 18:36:49 +0800

    一个简单的三层全连接网络

---
+++ TF_graph.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+# 三层神经网络，输入->隐藏->输出
+# 节点权值w1,w2
+w1 = tf.Variable(tf.random_norm((2,3),mean=0,stddev=2,seed=1))
+w2 = tf.Variable(tf.random_norm((3,1),mean=0,stddev=2,seed=1))
+
+# 输入值
+x = tf.constant([0.7,0.9],shape=[1,2])
+
+# 数据传递
+a = tf.matmul(x, w1)
+y = tf.matmul(a, w2)
+
+# 结果运行
+sess = tf.Session()
+# 权值初始化
+sess.run(w1.initializer)
+sess.run(w2.initializer)
+'''
+初始化所有变量：
+init_op = tf.global_variable_initializer()
+sess.run(init_op)
+这种初始化方式会自动处理变量间的依赖关系
+'''
+print(sess.run(y))
+sess.close()
